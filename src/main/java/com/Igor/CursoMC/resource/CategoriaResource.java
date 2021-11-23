b1d621411cb1c9959a58b34d1598e5a5a9e47132
igorthiago5 <igorthiago255@hotmail.com>
2021-11-22 23:51:29 -0400

    testando a primeria classe de dominio

--- src/main/java/com/Igor/CursoMC/resource/CategoriaResource.java
+++ src/main/java/com/Igor/CursoMC/resource/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.Igor.CursoMC.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Igor.CursoMC.domain.Categoria;
 
 
 
@@ -14,8 +19,14 @@ public class CategoriaResource {
 	
 	//A informação será recuperada via get(barra de navegação)
 	@RequestMapping(method=RequestMethod.GET)
-	public String retornar() {
-		return "retorno ok";
+	public List<Categoria> Lista() {
+		Categoria c1 = new Categoria(1,"Informatica");
+		Categoria c2 = new Categoria(2,"Escritótio");
+		
+		List lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
 
 }
