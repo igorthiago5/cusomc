474f2c19f2e4740160375a9600105fd26d221777
igorthiago5 <igorthiago255@hotmail.com>
2021-11-23 23:22:24 -0400

    Inserido elementos no banco de dados

--- src/main/java/com/Igor/CursoMC/CursoMcApplication.java
+++ src/main/java/com/Igor/CursoMC/CursoMcApplication.java
@@ -1,13 +1,32 @@
 package com.Igor.CursoMC;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.Igor.CursoMC.domain.Categoria;
+import com.Igor.CursoMC.repositories.CategroiaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategroiaRepository repo;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+		
+		
+		
+	}
+	//permite cria elemntos para inserir elemntos no BD
+	public void run(String...  args)throws Exception{
+		Categoria c1  = new Categoria(null,"Escritorio");
+		Categoria c2  = new Categoria(null,"Informátia");
+		
+		repo.saveAll(Arrays.asList(c1, c2)); 
 	}
 
 }
